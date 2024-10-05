e0cfbf189f3e829c90802b32b43f6934aba4b2c1
Akshat Dwivedi <akshatdwi755@gmail.com>
2024-10-05 14:23:58 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants"
-import profilePic from "../assets/kevinRushprofile.png";
+import profilePic from "../assets/kevinRushprofile.jpg";
 
 const Hero = () => {
   return <div className="border-b border-neutral-900 pb-4 lg:mb-35">
