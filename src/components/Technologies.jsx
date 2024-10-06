e4722462ab03908841e7796f50030d15edc924e2
Akshat Dwivedi <akshatdwi755@gmail.com>
2024-10-06 18:26:44 +0530

    animations

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -28,8 +28,17 @@ const iconVariants =(duration)=>({
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24"> 
-    <h2 className="my-20 text-center text-4xl">Technologies</h2>
-    <div className="flex flex-wrap items-center justify-center gap-4 ">
+    <motion.h2 
+    whileInView={{opacity:1 , y:0 }}
+    initial = {{opacity :0, y: -100}}
+    transition={{duration:1.5}} 
+    className="my-20 text-center text-4xl">Technologies</motion.h2>
+    <motion.div 
+    whileInView={{opacity :1 , x:0 }}
+    intial ={{opacity :0, x: -100}}
+    transition={{duration: 1.5}} 
+
+    className="flex flex-wrap items-center justify-center gap-4 ">
         <motion.div
         variants={iconVariants(2.5)}
         intial="inital"
@@ -73,7 +82,7 @@ const Technologies = () => {
             <BiLogoPostgresql className="text-7xl text-sky-700"/>
         </motion.div>
 
-    </div>
+    </motion.div>
     </div>
   )
 }
