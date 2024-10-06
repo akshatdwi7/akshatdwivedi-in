cc2591bc9dfa86a9d65fc8293215e5c072b25f90
Akshat Dwivedi <akshatdwi755@gmail.com>
2024-10-06 17:19:28 +0530

    feramer additon

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,33 @@
 import { HERO_CONTENT } from "../constants"
 import profilePic from "../assets/kevinRushProfile.jpg";
+import {  motion } from "framer-motion"
 
+
+const container =(delay) =>({
+  hidden: {x: -100, opacity : 0},
+  visible: {
+    x:0 , 
+    opacity: 1,
+    transition: {duration :0.4, delay:delay},
+  },
+});
 const Hero = () => {
   return <div className="border-b border-neutral-900 pb-4 lg:mb-35">
     <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
         <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
-                Akshat Dwivedi</h1>
-                <span className=" bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight 
-                text-transparent"> Full Stack Developer </span>
+            <motion.h1
+             variants={container((0))}
+             initial="hidden"
+             animate="visible"
+
+              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 
+            lg:text-8xl">
+             Akshat Dwivedi
+                </motion.h1>
+                <motion.span 
+  className=" bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight 
+                text-transparent"> Full Stack Developer </motion.span>
                 <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
                     {HERO_CONTENT}
                 </p>
