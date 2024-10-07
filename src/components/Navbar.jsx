0609f3e6573ced3d4f1ec48c2f463e4b777b2da2
Akshat Dwivedi <akshatdwi755@gmail.com>
2024-10-08 00:34:00 +0530

    yes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,15 +4,24 @@ import { FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
 const Navbar = () => {
+
+
   return <nav className="  mb-20 flex items-center justify-between py-6">
     <div className=" flex flex-shrink-0 items-center">
         <img className="mx-2 w-10 " src={logo} alt="logo" />
     </div>
     <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-      <FaLinkedin/>
-      <FaGithub/>
-      <FaSquareXTwitter/>
-      <FaInstagram/> 
+
+      <a href="https://www.linkedin.com/in/akshat-dwivedi-2497622a2/" target="_blank" rel="noopener noreferrer ">
+      < FaLinkedin/>
+        </a>
+<a href="https://github.com/akshatdwi7"  target="_blank" rel="noopener noreferrer ">
+<FaGithub/>
+</a>
+    <a href="https://x.com/Akshatdwi7"  target="_blank"  rel="noopener noreferrer "> 
+      <FaSquareXTwitter/></a>
+     <a href="https://www.instagram.com/nachosz/" target="_blank"  rel="noopener noreferrer">  <FaInstagram/> </a>
+    
     </div>
   </nav>
   
--- src/constants/index.js
+++ src/constants/index.js
@@ -3,9 +3,9 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 2 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With  years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
@@ -30,11 +30,11 @@ export const EXPERIENCES = [
     technologies: ["Python", "Svelte", "Three.js", "Postgres"],
   },
   {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
+    year: "2021-2022",
+    role: "Freelance",
+    company: "N/A",
     description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    technologies: ["HTML", "CSS", "JS", ],
   },
 ];
 
@@ -70,7 +70,6 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "Lucknow, India, 📍 ",
+  email: "akshatdwi755@gmail.com",
 };
