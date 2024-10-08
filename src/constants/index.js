c4fe71b28ab933c7db5d2b624f1c5673737313ef
Akshat Dwivedi <akshatdwi755@gmail.com>
2024-10-08 23:50:54 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,31 +9,35 @@ export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer w
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: " Present",
+    role: "Founder and Developer ",
+    company: "Equity Engage",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented Restfull
+     APIs At Equity Engage  , 
+     with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB" ,"Python"],
   },
   {
-    year: "2022 - 2023",
+    year: "2024",
     role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+    company: "SJ Innovations",
+    description: `Designed and developed user interfaces for web applications
+     using Next.js and React. Worked closely with backend developers to
+      integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL" ,"Js"],
   },
   {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+    year: "2024",
+    role: "Cloud Intern ",
+    company: "DCI",
+    description: `Developed and maintained web applications piplelines using AWS .`,
+    technologies: ["Python", "EC2", "S3", "AWS"  ,"IAM"],
   },
   {
-    year: "2021-2022",
+    year: "2023",
     role: "Freelance",
     company: "N/A",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    description: `Contributed to the development of web applications of clients and Made basic projects using FrontEnd-Stack.`,
     technologies: ["HTML", "CSS", "JS", ],
   },
 ];
